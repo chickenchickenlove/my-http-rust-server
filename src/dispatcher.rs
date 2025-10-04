c3a8fa1cae82b4cbee33eaefccc6acd508f3ed3b
chickenchickenlove <ojt90902@naver.com>
2025-10-04 19:01:53 +0900

    Migrate handler from sync to async.

--- src/dispatcher.rs
+++ src/dispatcher.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 use std::fmt::Debug;
 use std::future::Future;
 use std::pin::Pin;
-use std::process::Output;
+use std::sync::Arc;
 use tokio::net::TcpStream;
 use crate::connection::{ConnectionOwner};
 use crate::connection_reader::{HttpConnectionContext};
@@ -16,17 +16,29 @@ pub struct Dispatcher {
     router: Router
 }
 
-// TODO: Handler Function을 Future로 바꿔야 함.
-pub type BoxFuture<'a, T> = Pin<Box<dyn Future<Output=T> + Send + 'a>>;
-pub type Handler = fn(HttpRequest, HttpResponse) -> BoxFuture<'static, Result<HttpResponse>>;
-// pub type Handler = fn(HttpRequest, HttpResponse) -> Result<HttpResponse>;
+// TODO: 동적 디스패치없이 monomorphization으로 처리가 가능한 방법이 있는지 검토.
+// 1. Arc<T>로 해야한다. -> Rc<T>로 하게 되면 tokio::spawn(...) 내에서 사용될 Handler가 !Send 이다. 그러나 Tokio::spawn(...)는 Send + 'static을 요구하기 때문에 컴파일 에러가 발생한다.
+// 2. Trait Fn(...) -> ...은 dyn을 쓰지 않으면 단 하나의 타입만 의미한다. 동적 디스패치를 위해서 dyn Fn(...)을 사용한다.
+// 3. Type Alias에 Generic을 선언하는 것은 타입 강제력이 약하다. 보통은 사용하는 함수/구조체에서 바운드를 직접 거는 형식이 된다. Type Bound를 타입 자체에 넣고 싶다면 Trait Object를 사용한다.
+// 4. 아래 Trait Object는 Send + Sync도 구현해야한다는 것을 의미한다. 그리고 다양한 Trait Object Fn이 올 수 있음을 의미한다.
+
+pub type Handler = Arc<
+    dyn Fn(HttpRequest, HttpResponse)
+        -> Pin<Box<dyn Future<Output = Result<HttpResponse>> + Send + 'static>>
+    + Send
+    + Sync
+    // + 'static
+>;
+
+
 
 impl Dispatcher {
     pub fn new() -> Dispatcher {
         Dispatcher { router : Router::new() }
     }
 
-    pub fn add(&mut self, method: Method, path: &str, handler: Handler) -> Result<()> {
+    pub fn add(&mut self, method: Method, path: &str, handler: Handler) -> Result<()>
+    {
         Ok(self.router.add(method, path, handler)?)
     }
 
@@ -37,11 +49,11 @@ impl Dispatcher {
         let mut owner = ConnectionOwner::new(tcp_stream);
         let conn_context = owner.handle().await?;
 
-        let res = match conn_context {
+        let res: Result<HttpResponse> = match conn_context {
             HttpConnectionContext::HTTP1Context(ctx) => {
                 let req: HttpRequest = ctx.clone_req_ctx().into();
                 if let Some(handler) = self.router.find(req.method, req.path.as_str()) {
-                    handler(req, HttpResponse::new())
+                    Ok(handler(req, HttpResponse::new()).await?)
                 }
                 else {
                     Ok(HttpResponse::with_status_code(HttpStatus::NotFound))
@@ -104,7 +116,8 @@ impl Router {
         }
     }
 
-    pub fn add(&mut self, method: Method, path: &str, handler: Handler) -> Result<()> {
+    pub fn add(&mut self, method: Method, path: &str, handler: Handler) -> Result<()>
+    {
         if !path.starts_with("/") {
             // API를 제공하는 입장이기 때문에 Result를 반환해서 에러를 처리하도록 선택지를 주는 것이 낫다.
             bail!("Invalid path: {}. path should starts with '/'", path);
@@ -127,7 +140,8 @@ impl Router {
         }
     }
 
-    fn add_(routes: &mut HashMap<String, Handler>, path: &str, handler: Handler) -> Result<()>{
+    fn add_(routes: &mut HashMap<String, Handler>, path: &str, handler: Handler) -> Result<()>
+    {
         if routes.contains_key(path) {
             let msg = format!("{} already exists, so it will be overrided.", path);
             bail!(msg)
